4bdbd8309e4b50406728482836b50cc7501f75b1
Gurtaj <gurtaj@gmail.com>
2018-03-11 19:31:00 +0000

    Add bootstrap jQuery script tag back into index.html

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,10 +17,10 @@ class Footer extends React.Component {
                                 <div className="profile">
                                     <h6>DEVELOPER NETWORK</h6>
                                     <div className="networkIcons">
-                                        <a href="mailto:gurtajkang@yahoo.co.uk"><i className="fa fa-envelope" aria-hidden="true"></i></a>
+                                        <a href="mailto:gurtajkang@yahoo.co.uk"><i className="fa fa-envelope"></i></a>
                                         <a href="https://github.com/gurtaj1"><i className="fa fa-github"></i></a>
                                         <a href="http://www.linkedin.com/in/gurtaj-singh-kang-6883515b"><i className="fa fa-linkedin-square"></i></a>
-                                        <a href="https://stackoverflow.com/users/8383176/gurtaj"><i className="fa fa-stack-overflow fa-3x" aria-hidden="true"></i></a>
+                                        <a href="https://stackoverflow.com/users/8383176/gurtaj"><i className="fa fa-stack-overflow"></i></a>
                                     </div>
                                 </div>
                             </div>
